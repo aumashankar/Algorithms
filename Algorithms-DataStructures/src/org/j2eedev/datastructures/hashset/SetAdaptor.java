7014b59dd54d71899c625d73e002cb6ca99a8e93
Umashankar <umashankar111@gmail.com>
2014-05-19 09:57:10 +0530

    Set implementation in java

---
+++ Algorithms-DataStructures/src/org/j2eedev/datastructures/hashset/SetAdaptor.java
@@ -0,0 +1,46 @@
+package org.j2eedev.datastructures.hashset;
+
+import org.j2eedev.datastructures.hashmap.Map;
+import org.j2eedev.datastructures.hashmap.MapImpl;
+
+public class SetAdaptor<K> implements Set<K> {
+	
+	 private transient Map<K,Object> map;
+	 // Dummy value to associate with an Object in the backing Map
+	 private static final Object PRESENT = new Object();
+	 
+	 public SetAdaptor() {
+	        map = new MapImpl<K,Object>();
+	 }
+
+	@Override
+	public boolean add(K e) {
+		// TODO Auto-generated method stub
+		return  map.put(e, PRESENT)==null;
+	}
+
+	@Override
+	public boolean remove(K o) {
+		// TODO Auto-generated method stub
+		return (Object)map.remove(o)==PRESENT;
+	}
+
+	@Override
+	public boolean contains(K o) {
+		// TODO Auto-generated method stub
+		return map.get(o)!=null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return map.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return map.size()==0;
+	}
+
+}
