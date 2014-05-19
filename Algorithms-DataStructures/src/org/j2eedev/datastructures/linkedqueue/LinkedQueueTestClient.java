c87ece3a1ecf3625a1b86fa038f25b93e4100fde
Umashankar <umashankar111@gmail.com>
2014-05-19 09:56:00 +0530

    queue implementation with linked list

---
+++ Algorithms-DataStructures/src/org/j2eedev/datastructures/linkedqueue/LinkedQueueTestClient.java
@@ -0,0 +1,40 @@
+package org.j2eedev.datastructures.linkedqueue;
+
+import java.util.Iterator;
+
+import org.j2eedev.datastructures.linkedstack.Stack;
+
+public class LinkedQueueTestClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create a new Queue for Strings
+		Queue<String> myQueue=new Queue<String>();
+		System.out.println("Is my Queue empty?"+myQueue.isEmpty());
+		//enqueuing stings on to my queue.
+		myQueue.enqueue("Cormen");
+		myQueue.enqueue("Sahni");
+		myQueue.enqueue("Sedgewick");
+		myQueue.enqueue("Lafore");
+		myQueue.enqueue("Umashankar");
+		
+		//dequeue operation
+		myQueue.dequeue();
+		
+		System.out.println("No.of Strings in my queue "+myQueue.size());
+		
+		Iterator<String> itr=myQueue.iterator();
+		while (itr.hasNext()) {
+			String myString = (String) itr.next();
+			System.out.print(myString+" ");
+		}
+		//a little beautification of output!
+		System.out.println();
+		System.out.println("Least Recently added string "+myQueue.peek());
+
+	}
+
+}
