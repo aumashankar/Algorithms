71908db05f6f3e95f2dd449890fe0fdbcb4e50fd
Umashankar <umashankar111@gmail.com>
2014-05-19 09:44:02 +0530

    factorial using recursion in java

---
+++ Algorithms-DataStructures/src/org/j2eedev/recursion/FactorialUsingRecursion.java
@@ -0,0 +1,39 @@
+package org.j2eedev.recursion;
+
+public class FactorialUsingRecursion {
+
+	/**
+	 * <p>Factorial using Recursion</p>
+	 * @author Umashankar
+	 * {@link http://j2eedev.org}
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(factorial(3));
+		System.out.println(factorial(0));
+		System.out.println(factorial(-7));
+ 
+	}
+	/**
+	 * Recursion :: A method calls to it self
+	 * <p>Mathematical defintion of factorial</p>
+	 * 
+	 * 		f(n)= 1          if  n=0;
+	 * 		f(n)= n*f(n-1)   if  n>0;
+	 * 
+	 * @param n
+	 */
+	public static int factorial(int n){
+		//base case
+		if(n==0)
+			return 1;
+		//recursive case
+		else if(n>0)
+			return n*factorial(n-1);
+		//for negative numbers and non integers factorial is undefined.
+		else
+			return -1;
+	}
+
+}
