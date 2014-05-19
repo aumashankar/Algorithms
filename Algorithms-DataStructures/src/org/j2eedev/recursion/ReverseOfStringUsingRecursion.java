e92af5e381be0928980a41fa840bfd36b0d629bc
Umashankar <umashankar111@gmail.com>
2014-05-19 09:51:21 +0530

    reverse of a string using recursion

---
+++ Algorithms-DataStructures/src/org/j2eedev/recursion/ReverseOfStringUsingRecursion.java
@@ -0,0 +1,32 @@
+package org.j2eedev.recursion;
+
+public class ReverseOfStringUsingRecursion {
+
+	/**
+	 * @author Umashankar
+	 * {@link http://j2eedev.org}
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="This is Recursion Tutorial";
+		System.out.println(reverseString(s));
+ 	}
+	/**
+	 * <p> Reverse of a String using Recursion</p>
+	 * @param str
+	 * @return
+	 */
+	public static String reverseString(String str) {
+
+        //base case to handle one char string and empty string
+        if (str.length() < 2) {
+            return str;
+        }else{  //recursive case
+        return reverseString(str.substring(1)) + str.charAt(0);
+        }
+
+    }
+
+}
