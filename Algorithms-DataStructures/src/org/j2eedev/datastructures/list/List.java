a85932e649939462f716e3fc6074d4e914ad2dda
Umashankar <umashankar111@gmail.com>
2014-05-19 09:54:42 +0530

    list implementation in java

---
+++ Algorithms-DataStructures/src/org/j2eedev/datastructures/list/List.java
@@ -0,0 +1,52 @@
+package org.j2eedev.datastructures.list;
+/**
+ * A Simple List ADT
+ * @author Umashankar
+ * {@link http://j2eedev.org}
+ *
+ */
+public interface List {
+      /**
+       * Add item  
+       * @param obj
+       */
+	  public void add(Object obj);
+	  /**
+	   * Add item at position
+	   * @param pos
+	   * @param obj
+	   */
+	  public void add(int pos, Object obj);
+	  /**
+	   * Remove item at position
+	   * @param pos
+	   * @return
+	   */
+	  public Object remove(int pos);
+	  /**
+	   * Get Object at position
+	   * @param pos
+	   * @return
+	   */
+	  public Object get (int pos);
+	  
+	  //Other auxiliary operations
+	  /**
+	   * Search for an object, if it contains return true, else return false
+	   * @param Obj
+	   * @return
+	   */
+	  public boolean contains(Object Obj);
+	  /**
+	   * Size of the list
+	   * @return
+	   */
+	  public int size();
+	  /**
+	   * Return true if list is empty.
+	   * @return
+	   */
+	  public boolean isEmpty(); 
+	  
+	  public Object[] toArray();
+}
